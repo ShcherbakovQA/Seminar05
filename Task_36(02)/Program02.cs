c652d0abf42ad0f6f256669b411bd8820b66745a
Alexander <alex17.05.84@yandex.ru>
2022-10-28 18:39:37 +0300

    Task 36 done

---
+++ Task_36(02)/Program02.cs
@@ -0,0 +1,36 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+void HaveMassiv(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-1000, 1001);
+    }
+}
+void TypeArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+int HaveSum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum+=arr[i];
+        }
+    }
+    return sum;
+}
+
+int[] massiv = new int[4];
+HaveMassiv(massiv);
+TypeArray(massiv);
+Console.WriteLine("Сумма чисел на нечетных позициях = " + HaveSum(massiv));
+

caf47a6f1a7be1c3d2c49845bcecae6149c9e64b
Alexander <alex17.05.84@yandex.ru>
2022-10-28 18:48:21 +0300

    Task 38 done

---
+++ Task_38(03)/Program03.cs
@@ -0,0 +1,41 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите 
+// разницу между максимальным и минимальным элементами массива.
+
+void FillingMassiv(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-100, 101);
+    }
+}
+void TypeMassiv(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+void Differ(double[] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
+        if (min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+    Console.WriteLine("Разница между максимальным числом " + max + " и минимальным числом " + min + " = " + (max - Math.Abs(min)));
+}
+double[] massiv = new double[5];
+FillingMassiv(massiv);
+TypeMassiv(massiv);
+Differ(massiv);
+
+
